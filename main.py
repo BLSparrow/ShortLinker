d9a65c8f829939f76a70a2d2b7d2d8bb48c4f33c
BLSparrow <81457809+BLSparrow@users.noreply.github.com>
2021-12-28 20:08:14 +0300

    fake update

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from werkzeug.utils import redirect
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from UserLogin import UserLogin
 import bd
-import pyshorteners
-import pyperclip
 bd.db_create()
 
 app = Flask(__name__)
@@ -27,18 +25,11 @@ def load_user(user_id):
 menu = [
 {'name': 'Регистрация', 'url': 'register'},
 {'name': 'Авторизация', 'url': 'login'},
-{'name': 'Мои ссылки', 'url': 'office'},
-{'name': 'VIP ссылки', 'url': 'vip'},]
+{'name': 'Мои ссылки', 'url': 'office'},]
 
 @app.route('/', methods=['POST', 'GET']) 
 def index(): 
     print(url_for('index'))
-    links = bd.db_getLinkPublic()
-    if request.method == 'POST':
-        url = request.form.get('link')
-        link = pyshorteners.Shortener().tinyurl.short(url)
-        pyperclip.copy(link)
-        flash(f'Ваша новая ссылка: {link}')
     return render_template('index.html', links = links,  menu = menu) 
 
 
@@ -59,15 +50,12 @@ def register():
                 return redirect(url_for('login'))
         else:
             flash('Неверное заполнение полей! (Пароль>3 и Логин>0)')
-
     return render_template('register.html', title='Регистрация', menu = menu) 
 
 @app.route('/login', methods=['POST', 'GET']) 
 def login(): 
     if current_user.is_authenticated:
         return redirect(url_for('office'))
-
-    print(url_for('login'))
     if request.method == 'POST': 
         user = bd.getUserByLogin(request.form['login']) 
         if user and check_password_hash(user[2], request.form['password']):
@@ -75,9 +63,7 @@ def login():
             rm = True if request.form.get('remainme') else False
             login_user(userLogin, remember=rm)
             return redirect(request.args.get('next') or url_for('office'))
-            
         flash('Неверная пара логин/пароль!')
-     
     return render_template('login.html', title='Авторизация', menu = menu) 
 
 
@@ -94,54 +80,12 @@ def logout():
 @login_required
 def office(): 
     try:
-        user = bd.getUser(current_user.get_id())
         login = user[1]
-        status = request.form.get('status')
-        psevdonim = request.form.get('psevdonim')
-        url = request.form.get('link')
-        link = pyshorteners.Shortener().tinyurl.short(url)
-        pyperclip.copy(link)
-        bd.db_short(user[0], psevdonim, link, status)
-        flash(f'Ваша новая ссылка: {link}')
+        flash(f'Ваш личный кабинет')
     except:
-         flash('Неверная ссылка!')
+         flash('Что-то пошло не так!')
+    return render_template('office.html', title='Личный кабинет', login = login, menu = menu) 
 
-    links = bd.db_getLinkPrivate(user[0])
-    return render_template('office.html', title='Личный кабинет', login = login, links = links, menu = menu) 
-
-
-@app.route('/office/<int:id>/delete') 
-def delete(id):
-    user = bd.getUser(current_user.get_id())
-    try:
-        bd.db_deleteLink(user[0], id)
-        flash('Ссылка удалена!')
-        return redirect('/office')
-    except:
-        flash('При удалении произошла ошибка!')
-
-
-@app.route('/office/<int:id>/update',  methods=['POST', 'GET']) 
-def update(id):
-    link = bd.db_getLink(id)
-    if request.method == "POST":
-        status = request.form.get('status')
-        psevdonim = request.form.get('psevdonim')
-        try:
-            bd.db_update(psevdonim, status, id)
-            flash('Изменение прошло успешно!')
-            return redirect('/office')
-        except:
-            flash('При изменении произошла ошибка!')
-    else:
-        return render_template('update.html', title='Редактирование', link = link) 
-
-
-@app.route('/vip') 
-@login_required
-def vip(): 
-    linksOB = bd.db_getLinkOBD()
-    return render_template('vip.html', title='Ссылки общего доступа', linksOB = linksOB, menu = menu) 
 
 
 if __name__ == "__main__": 
